0c26e3195806a3ab40fe5883fadb801c3f584c0f
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-02-13 17:40:42 +0600

    05-JavaScript DOM CSS Style methods

---
+++ 05-JavaScript DOM CSS Style methods or How to get and set attributes in JavaScript/js/script.js
@@ -0,0 +1,5 @@
+const element = document.getElementById('heading2').style.color = 'red';
+const element1 = document.getElementById('heading2').style.backgroundColor = 'yellow';
+
+const element2 = document.getElementById('heading2');
+element2.style.textAlign = 'center';
