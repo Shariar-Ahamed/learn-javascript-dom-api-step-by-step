bd4a8d6945bbf201a70a88bb5565181ff7350849
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-02-13 11:45:28 +0600

    03-JavaScript DOM Selectors

---
+++ 03-JavaScript DOM Selectors/js/script.js
@@ -0,0 +1,8 @@
+const findElement = document.getElementById('hello');
+console.log(findElement);
+
+const findElement2 = document.getElementsByTagName('h2');
+console.log(findElement2);
+
+const findElement3 = document.getElementsByClassName('helloClass');
+console.log(findElement3);
