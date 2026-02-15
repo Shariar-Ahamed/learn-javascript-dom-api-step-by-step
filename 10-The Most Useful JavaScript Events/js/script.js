14c1756ac884179bd74a875271f34fdef5ef8b51
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-02-15 08:34:54 +0600

    10-The Most Useful JavaScript Events

---
+++ 10-The Most Useful JavaScript Events/js/script.js
@@ -0,0 +1,8 @@
+document.querySelector('#forClik').addEventListener('click', (e) => {
+        console.log(e)
+})
+
+
+document.getElementById('hadding-tag').addEventListener('dblclick', function(){
+        console.log('hello world')
+})
