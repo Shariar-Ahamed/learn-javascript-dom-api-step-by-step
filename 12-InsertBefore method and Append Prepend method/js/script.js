5d4d1a2af88fe9dd7fae584a9c3b5c5f1844f4c2
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-02-19 16:53:49 +0600

    12-InsertBefore method and Append Prepend method

---
+++ 12-InsertBefore method and Append Prepend method/js/script.js
@@ -0,0 +1,22 @@
+/*const menuTag = document.getElementById('menu');
+
+const li = document.createElement('li');
+li.textContent = 'First';
+
+menuTag.insertBefore(li, menuTag.firstChild);
+*/
+
+
+
+const menuTag1 = document.getElementById('menu-tag');
+
+const list = ['Home', 'About', 'Contact', 'Blog'];
+
+
+const makeList = list.map((ltems) => {
+    const li = document.createElement('li');
+    li.textContent = ltems;
+    return li;
+});
+
+menuTag1.append(...makeList);  //prepend {down}
